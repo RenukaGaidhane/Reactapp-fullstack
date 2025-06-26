1687743e2c94cb07a9cccd527d8952a83d7a4c5b
renukagaidhane <renukagaidhane100@gmail.com>
2025-06-26 21:18:44 +0530

    Add full frontend content and remaining folders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const fetchItems = async () => {
+    const res = await axios.get('http://localhost:5000/api/items');
+    setItems(res.data);
+  };
+
+  const addItem = async () => {
+    const res = await axios.post('http://localhost:5000/api/items', { name });
+    setItems([...items, res.data]);
+    setName('');
+  };
+
+  const deleteItem = async (id) => {
+    await axios.delete(`http://localhost:5000/api/items/${id}`);
+    setItems(items.filter(item => item._id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>REACT-APP</h1>
+      <input value={name} onChange={e => setName(e.target.value)} />
+      <button onClick={addItem}>Add</button>
+      <ul>
+        {items.map(item => (
+          <li key={item._id}>
+            {item.name} <button onClick={() => deleteItem(item._id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
