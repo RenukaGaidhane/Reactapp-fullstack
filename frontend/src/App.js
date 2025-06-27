088a6095f7b872e87691428935e41d5544b7fcc3
renukagaidhane <renukagaidhane100@gmail.com>
2025-06-27 18:51:55 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,7 +27,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>REACT-Deploy</h1>
+      <h1>REACT-APP</h1>
       <input value={name} onChange={e => setName(e.target.value)} />
       <button onClick={addItem}>Add</button>
       <ul>
